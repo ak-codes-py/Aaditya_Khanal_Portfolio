9cff4b6dc799b91b4aa0c4b063719c23376bc777
AadityaKhanal2003 <khanal.aditya1122@gmail.com>
2024-07-28 23:25:11 -0400

    test.

---
+++ profileDesign/profile.js
@@ -0,0 +1,46 @@
+/*!
+* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+document.addEventListener('DOMContentLoaded', function () {
+    var marqueeContainer = document.querySelector('.marquee-container');
+    var marqueeContent = `
+        <marquee behavior="scroll" direction="left" scrollamount="10">
+            <div class="flex items-center justify-center text-center">
+                <div class="flex flex-row p-2 m-2 max-w-none space-x-4">
+                    <!-- Your marquee content here -->
+                </div>
+            </div>
+        </marquee>`;
+    marqueeContainer.innerHTML = marqueeContent;
+});
